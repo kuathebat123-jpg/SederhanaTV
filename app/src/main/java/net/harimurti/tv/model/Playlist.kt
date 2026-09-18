7c1608354b30bbb7cec8113f8a3c1e562073fc81
kuathebat123-jpg <kuathebat123@gmail.com>
2026-09-18 12:53:36 +0800

    Update Playlist.kt

--- app/src/main/java/net/harimurti/tv/model/Playlist.kt
+++ app/src/main/java/net/harimurti/tv/model/Playlist.kt
@@ -3,12 +3,18 @@ package net.harimurti.tv.model
 import com.google.gson.annotations.SerializedName
 
 class Playlist {
-    var categories: ArrayList<Category> = ArrayList()
+
+    var categories: ArrayList<Category> =
+        ArrayList()
+
     @SerializedName("drm_licenses")
-    var drmLicenses: ArrayList<DrmLicense> = ArrayList()
+    var drmLicenses: ArrayList<DrmLicense> =
+        ArrayList()
 
     companion object {
+
         var cached = Playlist()
+
         var favorites = Favorites()
     }
 }
