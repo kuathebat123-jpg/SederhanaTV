ed199e7406cc984402f886f17e731e33d8ed9872
kuathebat123-jpg <kuathebat123@gmail.com>
2026-09-18 11:49:30 +0800

    Update DrmLicense.kt

--- app/src/main/java/net/harimurti/tv/model/DrmLicense.kt
+++ app/src/main/java/net/harimurti/tv/model/DrmLicense.kt
@@ -3,10 +3,22 @@ package net.harimurti.tv.model
 import com.google.gson.annotations.SerializedName
 
 class DrmLicense {
+
     @SerializedName("drm_id")
     var id: String? = null
+
     @SerializedName("drm_type")
-    lateinit var type: String
+    var type: String = ""
+
     @SerializedName("drm_key")
-    lateinit var key: String
+    var key: String = ""
+
+    /**
+     * HTTP headers yang dikirim ketika meminta license.
+     *
+     * Contoh:
+     * Authorization = Bearer xxxxx
+     * X-Custom-Header = value
+     */
+    var headers: HashMap<String, String> = HashMap()
 }
