416b767ca4d843b6f6b5fa528a522a91d7359fba
kuathebat123-jpg <kuathebat123@gmail.com>
2026-09-18 11:50:13 +0800

    Update ChannelRaw.kt

--- app/src/main/java/net/harimurti/tv/model/ChannelRaw.kt
+++ app/src/main/java/net/harimurti/tv/model/ChannelRaw.kt
@@ -1,12 +1,18 @@
 package net.harimurti.tv.model
 
 class ChannelRaw {
+
     var name: String? = null
     var group: String? = null
     var logoUrl: String? = null
     var streamUrl: String? = null
+
+    var manifestType: String? = null
+
     var drmType: String? = null
     var drmKey: String? = null
+    var drmHeaders: HashMap<String, String> = HashMap()
+
     var userAgent: String? = null
     var referer: String? = null
 }
