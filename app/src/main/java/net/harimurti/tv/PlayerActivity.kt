961f4fe6777812e30be66a5b06818f1a53d21211
kuathebat123-jpg <kuathebat123@gmail.com>
2026-09-18 15:18:58 +0800

    Update PlayerActivity.kt

--- app/src/main/java/net/harimurti/tv/PlayerActivity.kt
+++ app/src/main/java/net/harimurti/tv/PlayerActivity.kt
@@ -346,7 +346,7 @@ class PlayerActivity : AppCompatActivity() {
 
             setOnTouchListener(
                 object : OnSwipeTouchListener(
-                    this
+                    bindingRoot.playerView
                   ) {
 
                     override fun onSwipeDown() {
